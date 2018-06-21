c20d23863114e7dfdb57fea9745e14c07fd41009
BrandonHoeft <brandonmhoeft@gmail.com>
2018-06-21 18:15:01 -0500

    in which our hero makes more PROGRESS by writing  test code to iterate and combine elements from 2  lists in parallel (zip function).
     Also developed list comprehensions to format the
     columns that will need to be represented as plsql
     VARCHAR types and a comprehension to format the
     columns that will need to be represented as plsql
     DATE types.
    
     Effin' fun!

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,25 +1,88 @@
 read_path = '/Users/bhoeft/Desktop/temp/fake_simulated_data.csv'
 out_path = '/Users/bhoeft/Desktop/temp/output.txt'
-
+###############################################################################
 # Explore: captures headers row as a list, then prints each row line by line. 
+###############################################################################
+
 with open(read_path) as file:
     
     for i, line in enumerate(file): # file is an iterable.
         if i == 0:
             headers = line.rstrip('\n').split(',')
+        elif i == 1:
+            values = line.rstrip('\n').split(',')
+            
         else:
-            print(i, line)
+            print(line)
     
     print(headers)
             
 
+headers
+values
+for i, val in enumerate(values):
+    print(i, val)
+values[0]
+"".join(("'", values[0], "'"))
+# zip iterates over multiple objects in parallel. 
+varchar_indices = (0,1,2,3,6,9,10,12)
+date_indices = (13,14,15)
+
+# good! need to convert string value columns to varchar plsql format.
+values_as_varchar = ["".join(["'", val, "'"]) for i, val in enumerate(values) if i in varchar_indices]
+# Check output
+for i, char in enumerate(values_as_varchar):
+    print(i, char)
+
+
+# good! need to convert string value columns to varchar plsql format.
+values_as_date = ["".join(["TO_DATE('", val, "', ", "'mm/dd/yyyy')"]) for i, val in enumerate(values) if i in date_indices]
+for i, date in enumerate(values_as_date):
+    print(i, date)
+
+
+# good! need to combine value with header in a string. 
+[val + ' AS ' + header for val, header in zip(values, headers)]
+
+
+
+
+
+
+
+
+
+
+###############################################################################
+# Development of query output.  
+###############################################################################
+
+with open(read_path) as file:
     
+    for i, line in enumerate(file): # file is an iterable.
+        if i == 0:
+            headers = line.rstrip('\n').split(',')
+        elif i == 1:
+            values = line.rstrip('\n').split(',')
+            value_header_list = [val + ' AS ' + header 
+                                 for val, header in zip(values, headers)]
+            
+            'SELECT FROM dual UNION ALL'.format()
+        else:
+            print(line)
+    
+    print(headers)
 
 
 
 
 
+
+
+###############################################################################
 # Template structure for reading line by line and writing line by line.
+###############################################################################
+
 with open(read_path) as file:
     with open(out_path, 'a') as file_out: 
         for line in file:
@@ -27,12 +90,14 @@ with open(read_path) as file:
 
 
 
+datestr = '1956-01-31'
+year, month, day = datestr.split('-')
 
 
 
+'"Python"'
 
-
-
+"'Python'"
 
 
 
