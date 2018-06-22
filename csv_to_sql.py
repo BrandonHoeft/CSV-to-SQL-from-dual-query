14340883b621526cdefb0bea1cafac4afce8da2e
BrandonHoeft <brandonmhoeft@gmail.com>
2018-06-21 21:35:14 -0500

    in which our hero made more headway in taking  the formatting of VARCHAR and DATE sql types  and devised more code to be able to update the  original list of values if they were one of the  VARCHAR or DATE values. used the identified indices,  list.pop(), and list comprehensions to get this  achieved on experimental basis so far.
     Fug yeah!

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -11,10 +11,22 @@ with open(read_path) as file:
             headers = line.rstrip('\n').split(',')
         elif i == 1:
             values = line.rstrip('\n').split(',')
-            
+            # 1. need to format the anticipated SQL varchar elements.
+            # IN-PROGRESS BELOW
+            # 2. need to clean the anticipated SQL date type elements
+            # IN-PROGRESS BELOW
+            # 3. need to update the values list with cleaned varchar, date type vals
+            # TO DO
+            # 4. join the value with the header as query string
+            # IN-PROGRESS BELOW
+            # 5. format a select statement for the first FROM dual query.
+            # TO DO
+        elif i > 1:
+            None
         else:
             print(line)
     
+
     print(headers)
             
 
@@ -23,25 +35,44 @@ values
 for i, val in enumerate(values):
     print(i, val)
 values[0]
-"".join(("'", values[0], "'"))
-# zip iterates over multiple objects in parallel. 
+
+
+# identify values indices that need special formatting for a SQL statement. 
 varchar_indices = (0,1,2,3,6,9,10,12)
 date_indices = (13,14,15)
 
-# good! need to convert string value columns to varchar plsql format.
+# 1. need to format the anticipated SQL varchar elements. GOOD!
 values_as_varchar = ["".join(["'", val, "'"]) for i, val in enumerate(values) if i in varchar_indices]
 # Check output
 for i, char in enumerate(values_as_varchar):
     print(i, char)
 
 
-# good! need to convert string value columns to varchar plsql format.
+# 2. need to clean the anticipated SQL date type elements. GOOD!
 values_as_date = ["".join(["TO_DATE('", val, "', ", "'mm/dd/yyyy')"]) for i, val in enumerate(values) if i in date_indices]
 for i, date in enumerate(values_as_date):
     print(i, date)
 
 
-# good! need to combine value with header in a string. 
+
+
+# 3. need to update the values list with cleaned varchar, date type vals
+values_copy = list(values)
+values_as_varchar_copy = list(values_as_varchar)
+
+for i in range(len(values_copy)):    
+    if i in varchar_indices: # these were indices identified early to need VARCHAR formatting.
+        # from varchar formatted list, pop off first item and update i'th element of values.
+        values_copy[i] = values_as_varchar_copy.pop(0)
+values_copy
+# convert to a comprehension
+values_copy = [values_as_varchar_copy.pop(0) if i in varchar_indices else values_copy[i] for i in range(len(values_copy))]
+values_copy
+
+
+
+# 4. join the value with the header as query string. GOOD!
+# zip iterates over multiple objects in parallel. 
 [val + ' AS ' + header for val, header in zip(values, headers)]
 
 
