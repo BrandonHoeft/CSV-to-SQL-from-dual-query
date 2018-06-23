8626acf974535437f1d3a05bdcc3c3af06cdbf97
BrandonHoeft <brandonmhoeft@gmail.com>
2018-06-23 00:57:49 -0500

    in which our hero refactors the code to  improve readability by using custom cleaning  functions instead of multiple lengthy loops  and comprehensions within the WITH context  manager's code for input file and output file.
     NEXT Steps: convert into a function, with
     params like date_format, varchar_indices,
     date_indices, read_path, out_path

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,91 +1,8 @@
 read_path = '/Users/bhoeft/Desktop/CSV-to-SQL-from-dual-query/fake_simulated_data.csv'
 out_path = '/Users/bhoeft/Desktop/CSV-to-SQL-from-dual-query/output.txt'
 
-
-###############################################################################
-# Development of query output.  
-###############################################################################
-
-with open(read_path) as infile:
-    with open(out_path, 'a') as outfile:
-
-        varchar_indices = (0,1,2,3,6,9,10,12) 
-        date_indices = (13,14,15)
-        
-        # file is an iterable. can loop over indices and rows
-        for i, line in enumerate(infile): 
-
-            if i == 0:
-                # clean header values and unpack into list.
-                headers = line.rstrip('\n').lower().split(',')
-            elif i == 1:
-                # clean, unpack line into list of strings, splitting on delimiter
-                values = line.rstrip('\n').split(',')
-                print(values)
-                # format desired SQL VARCHAR elements by wrapping single-quotes 
-                # around the existing string
-                values_as_varchar = ["".join(["'", val, "'"]) 
-                                     for i, val in enumerate(values) 
-                                     if i in varchar_indices]
-                # format the SQL DATE elements to convert string to date in SQL.
-                values_as_date = ["".join(["TO_DATE('", val, "', ", "'mm/dd/yyyy')"]) 
-                                  for i, val in enumerate(values) 
-                                  if i in date_indices]
-                # update values list with cleaned varchar, date formatted vals
-                for i in range(len(values)):
-                    if i in varchar_indices:
-                        values[i] = values_as_varchar.pop(0)
-                print(values)
-                  
-                for i in range(len(values)):
-                    if i in date_indices:
-                        values[i] = values_as_date.pop(0)
-                # join each cleaned val with corresponding SQL field header
-                sql_select_list = [val + ' AS ' + header for val, header in zip(values, headers)]
-                # write initial FROM dual query. has headers.
-                outfile.write('SELECT ' + ', '.join(sql_select_list) + ' FROM DUAL UNION ALL \n')
-
-            elif i > 1:
-                # repeat above except but no need for HEADERS like initial query.  
-                values = line.rstrip('\n').split(',')
-                
-                values_as_varchar = ["".join(["'", val, "'"]) 
-                                     for i, val in enumerate(values) 
-                                     if i in varchar_indices]
-                
-                values_as_date = ["".join(["TO_DATE('", val, "', ", "'mm/dd/yyyy')"]) 
-                                  for i, val in enumerate(values) 
-                                  if i in date_indices]
-                
-                for i in range(len(values)):
-                    if i in varchar_indices:
-                        values[i] = values_as_varchar.pop(0)
-    
-                for i in range(len(values)):
-                    if i in date_indices:
-                        values[i] = values_as_date.pop(0)
-                
-                outfile.write('SELECT ' + ', '.join(values) + ' FROM DUAL UNION ALL \n')
-
-            else:
-                print('something unexpected happened. Investigate!')
-            
-            print('completed row ', i)
-
-
-
 ##############################################################################
-# Testing Code, then moving above.
-##############################################################################
-headers
-values
-for i, val in enumerate(values):
-    print(i, val)
-values[0]
-
-
-##############################################################################
-# Refactor code above to decompose into specific cleaning functions.
+# specific cleaning functions.
 ##############################################################################
 
 def string_to_sql_varchar(list_obj, list_varchar_indices):
@@ -120,11 +37,11 @@ def string_to_sql_date(list_obj, list_date_indices):
     
     return list_obj
 
-# testing
 
-##############################################################################
-# Update Code.
-##############################################################################
+###############################################################################
+# Development of CSV to SQL FROM dual temp table .txt file output.  
+###############################################################################
+
 
 with open(read_path) as infile:
     with open(out_path, 'a') as outfile:
@@ -141,25 +58,10 @@ with open(read_path) as infile:
             elif i == 1:
                 # clean, unpack line into list of strings, splitting on delimiter
                 values = line.rstrip('\n').split(',')
-                print(values)
-                # format desired SQL VARCHAR elements by wrapping single-quotes 
-                # around the existing string
-                values_as_varchar = ["".join(["'", val, "'"]) 
-                                     for i, val in enumerate(values) 
-                                     if i in varchar_indices]
-                # format the SQL DATE elements to convert string to date in SQL.
-                values_as_date = ["".join(["TO_DATE('", val, "', ", "'mm/dd/yyyy')"]) 
-                                  for i, val in enumerate(values) 
-                                  if i in date_indices]
-                # update values list with cleaned varchar, date formatted vals
-                for i in range(len(values)):
-                    if i in varchar_indices:
-                        values[i] = values_as_varchar.pop(0)
-                print(values)
-                  
-                for i in range(len(values)):
-                    if i in date_indices:
-                        values[i] = values_as_date.pop(0)
+                # clean, format desired SQL VARCHAR elements
+                values = string_to_sql_varchar(values, varchar_indices)
+                # clean, format the SQL DATE elements to convert string to date in SQL.
+                values = string_to_sql_date(values, date_indices)
                 # join each cleaned val with corresponding SQL field header
                 sql_select_list = [val + ' AS ' + header for val, header in zip(values, headers)]
                 # write initial FROM dual query. has headers.
@@ -168,29 +70,27 @@ with open(read_path) as infile:
             elif i > 1:
                 # repeat above except but no need for HEADERS like initial query.  
                 values = line.rstrip('\n').split(',')
-                
-                values_as_varchar = ["".join(["'", val, "'"]) 
-                                     for i, val in enumerate(values) 
-                                     if i in varchar_indices]
-                
-                values_as_date = ["".join(["TO_DATE('", val, "', ", "'mm/dd/yyyy')"]) 
-                                  for i, val in enumerate(values) 
-                                  if i in date_indices]
-                
-                for i in range(len(values)):
-                    if i in varchar_indices:
-                        values[i] = values_as_varchar.pop(0)
-    
-                for i in range(len(values)):
-                    if i in date_indices:
-                        values[i] = values_as_date.pop(0)
-                
+                values = string_to_sql_varchar(values, varchar_indices)
+                values = string_to_sql_date(values, date_indices)
                 outfile.write('SELECT ' + ', '.join(values) + ' FROM DUAL UNION ALL \n')
 
             else:
                 print('something unexpected happened. Investigate!')
             
             print('completed row ', i)
+
+
+
+
+##############################################################################
+# miscellaneous.
+##############################################################################
+headers
+values
+for i, val in enumerate(values):
+    print(i, val)
+values[0]
+
 
 
 
